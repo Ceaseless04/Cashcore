83ae6226200491f863d620da11f52d812a2926a6
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2024-11-04 13:24:35 -0500

    Added styling to loginpage  (#13)
    * added simple css styling to login page. Also deleted Component InputField for being useless lmfao
    
    * made the code less cringe by organizing the return statement
    
    * Began adding figma (deez) styling on the login page. Added the right centered login container and small changes to make the figma design
    
    * added finishing touches to login pages before pull from main

--- client/app/(tabs)/loginPage.tsx
+++ client/app/(tabs)/loginPage.tsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ImageBackground,
+} from "react-native";
 import { useState } from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function loginPage() {
   const [username, SetUserName] = useState("");
   const [password, SetPassword] = useState("");
-  const [rememberMe, SetRememberMe] = useState(0);
   const [errors, setErrors] = useState({});
 
   const onLoginButtonPress = () => {
@@ -12,108 +18,215 @@ export default function loginPage() {
 
     if (!username) errors.username = "Username is required!";
     if (!password) errors.password = "Password is required!";
-    if (rememberMe == 1) {
-      //enable cookies to remember login information
-    }
 
     //validation here
 
-    setErrors(errors);//if invalid user login attempt
+    setErrors(errors); //if invalid user login attempt
 
     return Object.keys(errors).length === 0;
   };
 
   return (
-    <View style={styles.container}>
-      <form>
-        <h2 style={styles.title}>Login</h2>
-        <TextInput
-          style={styles.inputFields}
-          placeholderTextColor={"#979797"}
-          textContentType="username"
-          placeholder="UserName"
-          value={username}
-          onChangeText={SetUserName}
-        ></TextInput>
-        {errors.username ? <Text>{errors.username}</Text> : null}
-        <TextInput
-          placeholderTextColor={"#979797"}
-          style={styles.inputFields}
-          textContentType="password"
-          placeholder="Password"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={SetPassword}
-        ></TextInput>
-        {errors.password ? <Text>{errors.password}</Text> : null}
-        <div style={styles.linksContainer}>
-          <div>
-            <input id="rememberMeBox" value={rememberMe} type="checkbox" />
-            Remember Me?
-          </div>
-          <div>
+    <>
+      <ImageBackground
+        source={require("../../assets/images/bg gradient.png")}
+        resizeMode="cover"
+        style={{ width: "100%" }}
+      ></ImageBackground>
+      <View style={styles.view}>
+
+
+        {/**
+         * fancy text thing on the left container
+         */}
+        <View style={{ flex: 1 }}>
+          <Text style={styles.bigTextLeft}>
+            Control <br />
+            <Text style={{ color: "#27CE78" }}>Your</Text>
+            <br />
+            Finances
+            <br />
+          </Text>
+        </View>
+        
+
+        {/**
+         * Log In page View container
+         */}
+        <View style={styles.container}>
+          <View
+            style={{ width: "60%", margin: "auto", justifyContent: "center" }}
+          >
+            <text style={styles.title}>
+              <img
+                style={{ maxWidth: 100 }}
+                src="https://cdn.britannica.com/06/150806-050-6AE99C98/Proboscis-monkey.jpg"
+              />{" "}
+              CashCore
+            </text>
+          </View>
+
+          {/**
+           * The h3 tag will have a check for if the use has been on the platform before, if the user has been on before, it will say welcome back, otherwise just say Create a new account
+           */}
+          <View>
+            <h1 style={{ textAlign: "center", paddingTop: "5%" }}>
+              Welcome Back!
+            </h1>
+          </View>
+
+          <View style={styles.linksContainer}>
+
+            {
+              //indication of error for username TextInput
+              errors.username ? (
+                <Text style={styles.error}>{errors.username}</Text>
+              ) : null
+            }
+            <TextInput
+              style={styles.inputFields}
+              placeholderTextColor={"#979797"}
+              textContentType="username"
+              placeholder="UserName"
+              value={username}
+              onChangeText={SetUserName}
+            ></TextInput>
+
+            {
+              //error indication for password TextInput
+              errors.password ? (
+                <Text style={styles.error}>{errors.password}</Text>
+              ) : null
+            }
+            <TextInput
+              style={styles.inputFields}
+              placeholderTextColor={"#979797"}
+              textContentType="password"
+              placeholder="Password"
+              value={password}
+              onChangeText={SetPassword}
+              secureTextEntry={true}
+            ></TextInput>
+
             <a
               style={styles.links}
               href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
             >
               Forgot Password?
             </a>
-          </div>
-        </div>
-        <Button
-          title="Log In"
-          accessibilityLabel="Log into this Cashcore with youre account"
-          onPress={onLoginButtonPress}
-        />
-        <div style={styles.linksContainer}>
-          <a
-            style={styles.links}
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          >
-            Don't Have an Account?
-          </a>
-        </div>
-      </form>
-    </View>
+
+            {/**
+                Login Button
+              */}
+
+            <TouchableOpacity
+              style={styles.Button}
+              onPress={onLoginButtonPress}
+            >
+              <Text style={{ fontSize: 24, color: "white" }}>Log In</Text>
+            </TouchableOpacity>
+
+            <a
+              style={styles.links}
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            >
+              Don't Have an Account?
+            </a>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: "50%",
-    maxWidth: 750,
-    color: "#979797",
+  view: {
+    width: "100%",
+    flexDirection: "row",
     margin: "auto",
     alignItems: "center",
+    justifyContent: "center",
+    flexShrink: 1,
+    textAlign: "center",
+    gap: 100,
+  },
+
+  bigTextLeft: {
+    fontSize: 100,
+    margin: "auto",
+    textAlign: "center",
+    color: "white",
+    fontWeight: 700,
+  },
+
+  container: {
+    flex: 1,
+    color: "white",
+    textAlign: "center",
     backgroundColor: "#3E3E3E",
-    borderRadius: 10,
-    padding: 10,
+    paddingTop: "5%",
+    paddingBottom: "10%",
+    padding: 0,
+    marginRight: "2%",
+    maxHeight: "90%",
+    width: "20%",
+    borderRadius: 20,
+    borderWidth: 1,
   },
 
   title: {
     textAlign: "center",
+    fontSize: 56,
+    fontWeight: 700,
   },
 
   inputFields: {
-    alignContent: "center",
-    width: "80%",
-    display: "flex",
-    margin: "10%",
+    height: "5%",
+    marginVertical: "5%",
+    borderRadius: 6,
     borderLeftWidth: 1,
     borderRightWidth: 1,
     borderTopWidth: 1,
     borderBottomWidth: 1,
     borderColor: "#979797",
     borderStyle: "solid",
+    fontSize: 18,
+    paddingLeft: 10,
+    paddingVertical: "2%",
   },
 
   linksContainer: {
-    marginTop: 20,
-    marginBottom: 20,
+    //backgroundColor:"red",
+    textAlign: "left",
+    width: "60%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingBottom: "20%",
+  },
+
+  Button: {
+    marginVertical: "2%",
+    //height:"10%",
+    paddingVertical: "1%",
+    borderRadius: 6,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    backgroundColor:
+      "linear-gradient(86deg, rgba(41,159,98,1) 0%, rgba(77,176,136,1) 100%)",
+    alignItems: "center",
   },
 
   links: {
-    alignContent: "center",
     color: "#979797",
+    marginTop: "5%",
+    marginBottom: "5%",
+    fontSize: 18,
+  },
+
+  error: {
+    color: "#CE277D",
+    fontSize: 18,
   },
 });
