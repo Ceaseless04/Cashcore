9518a09fd0e5e78649f381d6c5c56e6f707476be
Erick <ericklagreat@gmail.com>
2024-11-09 17:29:16 -0500

    clean-up

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -3,17 +3,24 @@
   Each block will take up the space of the screen.
   All blocks are wraped up by a parent container, where its only job is to allign all given blocks
 
-  Tomorrow: Work on header above with functionalities/continue onto block2
+  Tomorrow: Work on Block 2
 
 */
 
 import React, { useRef, useEffect } from "react";
-import { Text, View, StyleSheet, ScrollView, Dimensions, Animated, Easing } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Dimensions, Animated, Easing, TouchableOpacity } from "react-native";
 
-import {Svg, RadialGradient, Defs, Stop, Circle } from "react-native-svg";
+
+import { Svg, RadialGradient, Defs, Stop, Circle } from "react-native-svg";
+import SvgLogo from "./../assets/homepage/img/final-brand-logo.svg"; //works, but showing as errors on VSCode
+import SignUpBtnSvg from "./../assets/homepage/img/get-started.svg"; //works, but showing as errors on VSCode
+import LogInBtnSvg from "./../assets/homepage/img/login.svg";
+
+import Jumper from "./../components/homepage/jumper";
 
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
+import { Link } from "expo-router";
 
 
 //store window height and width
@@ -23,6 +30,8 @@ const window_width = Dimensions.get("window").width;
 //debug
 console.log("Win_H: ", window_height);
 console.log("Win_W: ", window_width);
+console.log("Win_Added: ", window_width * .049);
+
 
 const parent_styles = StyleSheet.create({
   wrap_all: { //center piece of the page
@@ -32,18 +41,62 @@ const parent_styles = StyleSheet.create({
   },
 });
 
+const h = StyleSheet.create({
+  head: {
+    width: "100%", 
+    height: "auto",
+  },
+  wrapper: {
+    width: "100%",
+    height: "auto",
+    padding: "1%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  logo: {
+    width: "18%",
+  },
+  nav_text_c: {
+    width: "60%",
+    flexDirection: "row", 
+    height: "auto",
+    justifyContent: "center",
+
+  },
+  nav_text:{
+    color: "#ffffff",
+    paddingLeft: "2%",
+    paddingRight: "2%",
+  },
+  b_c: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "18%",
+    height: "auto",
+  },
+  b1: {
+    width: 100,
+    marginRight: 30,
+  },
+  b2: {
+    width: 100,
+  }
+}); 
+
 //the first container on top
 const block1 = StyleSheet.create({
   wrapper: { //the "Control Your Finances"
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
-    height: window_height,
+    height: window_height - (window_height * 0.2),
   },
   //wrap around text and gradient
   c1: { 
     width: "40%",
-    height: "80%",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -83,14 +136,47 @@ const block2 = StyleSheet.create({
   },
 });
 
+//add navigation to other pages
+
 export default function HomePage() {
+
+  
 
   loadFonts();
   const circle_size = get_circle_size()
 
   return (
-    <ScrollView style={parent_styles.wrap_all} pagingEnabled>
+    <ScrollView style={parent_styles.wrap_all}>
       
+      <header style={h.head}>
+        <View style={h.wrapper}>
+          
+          <SvgLogo style={h.logo}></SvgLogo>
+          
+          <View style={h.nav_text_c}>
+              <Text style={h.nav_text}>About</Text>
+              <Text style={h.nav_text}>Resources</Text>
+              <Text style={h.nav_text}>Contact Us</Text>
+          </View>
+
+          <View style={h.b_c}>
+            <TouchableOpacity activeOpacity={0.7}>
+              <Link href="./pages/loginPage">
+                <LogInBtnSvg style={h.b2}/>
+              </Link>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={SignUpBtnSvg.onPress} activeOpacity={0.7}>
+              <Link href="./pages/signup">
+                <SignUpBtnSvg style={h.b1}/>
+              </Link>
+            </TouchableOpacity>
+          </View>
+
+          
+        </View>       
+      </header>
+
+
       <View style={block1.wrapper}>
         <View style={block1.c1}>
           <Animated.View style={[block1.gradient_container, {opacity: pulsate_gradiant()}]}>
@@ -111,23 +197,25 @@ export default function HomePage() {
       </View>
 
 
-
       <View style={block2.c2}>
         <Text style={block1.c1_g_text}>Testing Text</Text>
       </View>
 
+      <Jumper></Jumper>
     </ScrollView>
+
+    
   );
 }
 
 //loading fonts to be used on the hompage
 function loadFonts() {
   const [font_loaded] = useFonts({
-    "spaceGroteskBold": require("./../../assets/fonts/SpaceGrotesk-Bold.otf"),
+    "spaceGroteskBold": require("./../assets/fonts/SpaceGrotesk-Bold.otf"),
   });
 
   if (!font_loaded){
-    return <AppLoading/> //some tag, ================look it up later=================
+    return <AppLoading/> //some tag, ================look up definition=================
   } else {
     return font_loaded;
   }
@@ -172,3 +260,6 @@ function get_circle_size() {
     return (window_height * 0.8)/2;
   }
 }
+
+
+//debug button css
