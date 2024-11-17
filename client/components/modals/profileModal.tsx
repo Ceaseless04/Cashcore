8c6a48b5f2ae38f2dd031f35908b7c1b8a14a3d3
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air-2.local>
2024-11-17 11:41:52 -0500

    Started styling the modal still figuring out React Native best practices

--- client/components/modals/profileModal.tsx
+++ client/components/modals/profileModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Modal, View, Text, Pressable, StyleSheet } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 interface ProfileModalProps {
   isVisible: boolean;
@@ -11,32 +12,67 @@ const ProfileModal = ({
   onClose,
 }: ProfileModalProps): JSX.Element => {
   return (
-    <Modal
-      style={styles.modalContainer}
-      visible={isVisible}
-      onRequestClose={onClose}
-      animationType="fade"
-      transparent={true}
-    >
-      <View>
-        <Text>This is the profile modal!!!!!!!!</Text>
-        <Pressable onPress={onClose}>
-          <Text>Save</Text>
-        </Pressable>
-        <Pressable onPress={onClose}>
-          <Text>Close</Text>
-        </Pressable>
-      </View>
-    </Modal>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.centeredView}>
+        <Modal
+          style={styles.centeredView}
+          visible={isVisible}
+          onRequestClose={onClose}
+          animationType="slide"
+          transparent={true}
+        >
+          <View style={styles.modalContent}>
+            <Text>This is the profile modal!!!!!!!!</Text>
+            <Pressable style={styles.saveButton} onPress={onClose}>
+              <Text>Save</Text>
+            </Pressable>
+            <Pressable style={styles.cancelButton} onPress={onClose}>
+              <Text>Cancel</Text>
+            </Pressable>
+          </View>
+        </Modal>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 export default ProfileModal;
 
 const styles = StyleSheet.create({
-  modalContainer: {
+  centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  modalContent: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "gray", // TODO: change to theme color
+    height: "auto",
+    maxWidth: 500,
+  },
+  saveButton: {
+    padding: 4,
+    backgroundColor: "green", // TODO: change to theme color
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  cancelButton: {
+    padding: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
