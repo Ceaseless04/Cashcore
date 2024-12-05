fe6f46d0427ad1bfb75a191b55bf405c36d0f69c
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2024-12-04 19:16:56 -0500

    pretty much done with login page, the background is just a svg stretched out the webpage's size. It was the only solution we can up with.

---
+++ client/app/pages/savings.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function savings(){
+
+    return (
+
+        <View>
+            <View></View>
+        </View>
+
+    );
+
+    
+
+}
